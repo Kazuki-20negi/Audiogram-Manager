e80f81b962113851f3706a700d8e9d9d8b6c10d7
20本ネギ <s.kazuki0111@gmail.com>
2025-12-22 21:37:47 +0900

    和暦変換関数

--- main.py
+++ main.py
@@ -2,12 +2,43 @@ import os
 import re
 import pytesseract
 from PIL import Image
-from datetime import datetime
+import datetime
+import unicodedata
 from dotenv import load_dotenv
 # .envファイルを読み込む
 load_dotenv()
 TESSERACT_PATH=os.getenv("TESSERACT_PATH")
 pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
+
+# 各年号の元年を定義
+eraDict = {
+    "昭和": 1926,
+    "平成": 1989,
+    "令和": 2019,
+}
+
+def japanese_calendar_converter(text):
+    # 正規化
+    normalized_text = unicodedata.normalize("NFKC", text)
+
+    # 年月日を抽出
+    pattern = r"(?P<era>{eraList})(?P<year>[0-9]{{1,2}}|元)年(?P<month>[0-9]{{1,2}})月(?P<day>[0-9]{{1,2}})日".format(eraList="|".join(eraDict.keys()))
+    date = re.search(pattern, normalized_text)
+
+    # 抽出できなかったら終わり
+    if date is None:
+        print("Cannot convert to western year")
+
+    # 年を変換
+    for era, startYear in eraDict.items():
+        if date.group("era") == era:
+            if date.group("year") == "元":
+                year = eraDict[era]
+            else:
+                year = eraDict[era] + int(date.group("year")) - 1
+    
+    # date型に変換して返す
+    return datetime.date(year, int(date.group("month")), int(date.group("day")))
 
 image_file=Image.open("test.png")
 width, height = image_file.size
@@ -36,7 +67,10 @@ test_result=re.findall(pattern_,test_date)
 print(test_date)
 print(test_result)
 
-result_date=str(test_result[2]).replace(" ","")
-result_date=result_date.translate(str.maketrans({"年":"-","月":"-","日":None}))
+result_date=str(test_result[0]).replace(" ","")
+if "平成" in result_date or "令和" in result_date:
+    result_date=japanese_calendar_converter(result_date)
+else:
+    result_date=result_date.translate(str.maketrans({"年":"-","月":"-","日":None,"/":"-"}))
 
 print(result_date)
