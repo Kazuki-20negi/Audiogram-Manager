c0b30b3fd18c57a272ec4d097609c6520a2bb72a
20本ネギ <s.kazuki0111@gmail.com>
2025-12-22 20:24:16 +0900

    西暦の日付整形終わり

--- main.py
+++ main.py
@@ -14,18 +14,20 @@ width, height = image_file.size
 new_size=(width*2, height*2)
 image_file=image_file.resize(new_size, Image.Resampling.LANCZOS)
 
-#前処理2値化
-image_gray=image_file.convert("L")
-threshold=180
-image_gray=image_gray.point(lambda x: 0 if x<threshold else 255)
+#前処理2値化 本番で戻す
+#image_gray=image_file.convert("L")
+#threshold=180
+#image_gray=image_gray.point(lambda x: 0 if x<threshold else 255)
 
 #デバッグ用、本番では消す
-image_gray.save("test_gray.png")
+#image_gray.save("test_gray.png")
+image_gray=Image.open("test_gray.png")
 
 #読み取り
 custom_config=r"--psm 6"
 test_date=pytesseract.image_to_string(image_gray,lang="jpn+eng", config=custom_config)
 
+#正規表現パターン
 pattern_west=r"(?:\d{4}\s*[/年]\s*\d{1,2}\s*[/月]\s*\d{1,2}\s*日*)"
 pattern_jp = r"(?:(?:令和|平成)\s*\d{1,2}\s*年\s*\d{1,2}\s*月\s*\d{1,2}\s*日*)"
 pattern_=rf"{pattern_west}|{pattern_jp}"
@@ -34,6 +36,7 @@ test_result=re.findall(pattern_,test_date)
 print(test_date)
 print(test_result)
 
-result_date=str(test_result[1]).replace(" ","")
-result_date=result_date.replace("年","/")
+result_date=str(test_result[2]).replace(" ","")
+result_date=result_date.translate(str.maketrans({"年":"-","月":"-","日":None}))
+
 print(result_date)
