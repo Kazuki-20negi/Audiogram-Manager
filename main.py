d37aef65c4d4bd839fab9be77ab5b90c5adb9725
20本ネギ <s.kazuki0111@gmail.com>
2025-12-22 19:20:36 +0900

    change

--- main.py
+++ main.py
@@ -8,10 +8,22 @@ load_dotenv()
 TESSERACT_PATH=os.getenv("TESSERACT_PATH")
 pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
 
+image_file=Image.open("test.png")
+width, height = image_file.size
+new_size=(width*2, height*2)
+image_file=image_file.resize(new_size, Image.Resampling.LANCZOS)
 
+#前処理2値化
+image_gray=image_file.convert("L")
+threshold=180
+image_gray=image_gray.point(lambda x: 0 if x<threshold else 255)
 
-Image_=Image.open("test.png")
-test_date=pytesseract.image_to_string(Image_,lang="jpn+eng")
+#デバッグ用、本番では消す
+image_gray.save("test_gray.png")
+
+#読み取り
+custom_config=r"--psm 6"
+test_date=pytesseract.image_to_string(image_gray,lang="jpn+eng", config=custom_config)
 pattern_=r"\d*/\d*/\d*"
 test_result=re.findall(pattern_,test_date)
 
