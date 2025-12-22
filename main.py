c8adf8debd2af97bc3300fdad656dc06142707f2
20本ネギ <s.kazuki0111@gmail.com>
2025-12-22 18:25:55 +0900

    main update and add test date

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import os
 import re
 import pytesseract
+from PIL import Image
+from dotenv import load_dotenv
+# .envファイルを読み込む
+load_dotenv()
+TESSERACT_PATH=os.getenv("TESSERACT_PATH")
+pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
+
+
+
+Image_=Image.open("test.png")
+test_date=pytesseract.image_to_string(Image_,lang="jpn+eng")
+pattern=r"{\d}*/{\d}*/{\d}*"
+
+print(test_date)
